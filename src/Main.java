955def3170817483d57e02b5b96e434b7599433c
Davran2005 <davranjoldoshbaev2@gmail.com>
2022-12-07 18:23:36 +0600

    Conmmit

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<>(Arrays.asList(1,2,3));
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
         System.out.println("Set1: " + set1);
-        Set<Integer> set2 = new HashSet<>(Arrays.asList(0,1,2));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(0, 1, 2));
         System.out.println("set2: " + set2);
         System.out.println(symmetricDifference(set1, set2));
 
     }
 
-    public static Set<Integer> symmetricDifference(Set<Integer> set1, Set<Integer> set2) {
+    public static String symmetricDifference(Set<Integer> set1, Set<Integer> set2) {
         Set<Integer> set3 = new HashSet<>(set1);
         set3.addAll(set2);
         Set<Integer> set4 = new HashSet<>(set1);
         set4.retainAll(set2);
         set3.removeAll(set4);
-        return set3;
+        return "Ответ: " + set3;
     }
-    }
+}
